2c93a9b7ef58c9e00a68e20c8f06ce79fc0766f4
deantosh <deantoshdaiddoh@gmail.com>
2025-01-04 23:09:33 -0800

    Added: sidebar component

---
+++ client/src/components/sidebar.js
@@ -0,0 +1,37 @@
+const React = require('react');
+const { Link } = require('react-router-dom');
+
+const Sidebar = ({ rooms, unreadMessages, setSelectedRoom }) => {
+  return (
+    <div className="sidebar">
+      <h2>Your Rooms</h2>
+      <ul>
+        {rooms.map((room) => (
+          <li key={room._id}>
+            <Link to="#" onClick={() => setSelectedRoom(room._id)}>
+              {room.name}
+              {unreadMessages[room._id] > 0 && (
+                <span className="badge">
+                  {unreadMessages[room._id]}
+                </span>
+              )}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      {/* Link to create a new room page */}
+      <Link to="/create-room">
+        <button>Create New Room</button>
+      </Link>
+
+      {/* Link to view all rooms page */}
+      <Link to="/rooms">
+        <button>View All Rooms</button>
+      </Link>
+    </div>
+  );
+};
+
+// Export sidebar component
+module.exports = Sidebar;
