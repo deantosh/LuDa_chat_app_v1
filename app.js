368d2c58dc5f8dbbf66cf16109a3081e146cbb2a
luckys-lnz <lalucky22@gmail.com>
2025-01-07 20:12:05 +0000

    controllers

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = require('./routes/index');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from your frontend
+  methods: ['GET', 'POST', 'OPTIONS'], // Allow the necessary HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
+}));
+
 app.use(express.json());
 app.use(router);
+app.options('*', cors());
 
 const PORT = 5000;
 app.listen(PORT, () => {
