39e07c1c8bbd76f3fd906d10a72e9f1890ad82a3
luckys-lnz <lalucky22@gmail.com>
2025-01-10 18:12:50 +0000

    changed some stuffs on app.js

--- app.js
+++ app.js
@@ -9,12 +9,18 @@ app.use(cors({
   origin: 'http://localhost:3000', // Allow requests from your frontend
   methods: ['GET', 'POST', 'OPTIONS'], // Allow the necessary HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
+  credentials: true,
+  secure: false,
 }));
 
 app.use(express.json());
-app.use(router);
 app.use(cookieParser());
-app.options('*', cors());
+app.use(router);
+app.use((req, res, next) => {
+  console.log('Request Origin:', req.get('Origin'));
+  next();
+});
+// app.options('*', cors()); // Remove to avoid overriding your specific CORS configuration
 
 const PORT = 5000;
 app.listen(PORT, () => {
