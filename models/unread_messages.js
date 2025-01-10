ec5a6c042e0f9f47bca740cdaaaf943567dc9e13
luckys-lnz <lalucky22@gmail.com>
2025-01-10 18:19:28 +0000

    unread_messages

--- models/unread_messages.js
+++ models/unread_messages.js
@@ -3,12 +3,15 @@
  * Track unread messages for each user in each room.
  */
 const { mongoose } = require('../utils/db');
+const baseSchema = require('./base_model');
 
 const unreadMessagesSchema = new mongoose.Schema({
   roomId: { type: mongoose.Schema.Types.ObjectId, ref: 'Room' },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   unreadCount: { type: Number, default: 0 }
 });
+
+unreadMessagesSchema.add(baseSchema);
 
 const UnreadMessage = mongoose.model('UnreadMessage', unreadMessagesSchema);
 
