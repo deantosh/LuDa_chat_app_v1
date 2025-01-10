45586cf7ef23e55f817ce1e4d2f7f814c9bc43e7
luckys-lnz <lalucky22@gmail.com>
2025-01-10 18:18:14 +0000

    authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,40 +1,45 @@
-const { v4: uuidv4 } = require('uuid');
-const sha1 = require('sha1');
-const redisClient = require('../utils/redis');
-const User = require('../models/user');
+const { v4: uuidv4 } = require("uuid");
+const sha1 = require("sha1");
+const redisClient = require("../utils/redis");
+const User = require("../models/user");
 
 class AuthController {
   static async getConnect(req, res) {
     // Get Authorization header and verify it's not empty and is of Basic type
     const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Basic')) {
-      return res.status(401).json({ error: 'Missing or Invalid Authorization header' });
+    if (!authHeader || !authHeader.startsWith("Basic")) {
+      return res
+        .status(401)
+        .json({ error: "Missing or Invalid Authorization header" });
     }
 
     // Get authHeader value
-    const base64Credentials = authHeader.split(' ')[1];
+    const base64Credentials = authHeader.split(" ")[1];
 
     // Decode Base64 Header value to <email>:<password>
-    const decodedCredentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+    const decodedCredentials = Buffer.from(
+      base64Credentials,
+      "base64"
+    ).toString("utf-8");
 
     // Get email and password values
-    const [email, password] = decodedCredentials.split(':');
+    const [email, password] = decodedCredentials.split(":");
 
     // Find user using email
     let user;
     try {
       user = await User.findOne({ email });
       if (!user) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        return res.status(401).json({ error: "Unauthorized" });
       }
     } catch (err) {
-      return res.status(500).json({ error: 'Internal Server Error' });
+      return res.status(500).json({ error: "Internal Server Error" });
     }
 
     // Hash password and check if correct
     const hashedPassword = sha1(password);
     if (user.passwordHash !== hashedPassword) {
-      return res.status(401).json({ error: 'Unauthorized' });
+      return res.status(401).json({ error: "Unauthorized" });
     }
 
     // Create user token
@@ -48,23 +53,23 @@ class AuthController {
       await redisClient.set(authToken, user._id.toString(), 24 * 60 * 60); // TTL: 24 hours
 
       // Store the token in a cookie with HttpOnly and Secure flags
-      res.cookie('token', authToken, {
+      res.cookie("token", authToken, {
         httpOnly: true, // Cannot be accessed by JavaScript on the client side
-        secure: false, 
+        secure: false,
         maxAge: 24 * 60 * 60 * 1000, // Cookie expires in 24 hours
-        sameSite: 'None', // CSRF protection
+        sameSite: "Lax", // CSRF protection
       });
       return res.status(200).json({ token });
     } catch (error) {
-      return res.status(500).json({ error: 'Internal Server Error' });
+      return res.status(500).json({ error: "Internal Server Error" });
     }
   }
 
   static async getDisconnect(req, res) {
     // Get user token
-    const userToken = req.headers['x-token'];
+    const userToken = req.headers["x-token"];
     if (!userToken) {
-      return res.status(401).json({ error: 'Unauthorized' });
+      return res.status(401).json({ error: "Unauthorized" });
     }
 
     // Create key using token
@@ -74,14 +79,14 @@ class AuthController {
     try {
       const validToken = await redisClient.get(key);
       if (!validToken) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        return res.status(401).json({ error: "Unauthorized" });
       }
 
       // Delete token
       await redisClient.del(key);
       return res.status(204).json({});
     } catch (error) {
-      return res.status(500).json({ error: 'Internal Server Error' });
+      return res.status(500).json({ error: "Internal Server Error" });
     }
   }
 }
