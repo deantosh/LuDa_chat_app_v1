3fd3f792c0e05ebb2758180af674a18cad99157d
luckys-lnz <lalucky22@gmail.com>
2025-01-10 18:15:21 +0000

    modified js

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import '../styles/header.css';
+import axios from "axios";
 
-const Header = ({ user }) => {
+
+const Header = () => {
+  const [user, setUser] = useState(""); // To store the logged-in user ID
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/users/me", {
+          withCredentials: true,
+        });
+        setUser(response.data.user); // Set the logged-in user ID
+      } catch (error) {
+        console.error("Failed to fetch user:", error);
+        alert("Failed to fetch user details.");
+      }
+    };
+    fetchUser();
+  }, []); // Runs only once on component mount
   return (
     <header className="header">
       <div className="logo">
-        <h1>ChatApp</h1>
+        <h1>LuDa Chat</h1>
       </div>
       <div className="user-info">
         {user ? (
-          <span>Welcome, {user.name}</span>
+          <span>Welcome, {user.username}</span>
         ) : (
           <span>Welcome, Guest</span>
         )}
