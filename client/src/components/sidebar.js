61b83bec5e407d8d183aeb2d9c38420e371d9798
luckys-lnz <lalucky22@gmail.com>
2025-01-10 18:14:55 +0000

    sidebar

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import '../styles/sidebar.css';
+import axios from "axios";
 
-const Sidebar = ({ rooms, unreadMessages, setSelectedRoom }) => {
+
+const Sidebar = () => {
+  const [user, setUser] = useState("");
+  const [rooms, setRooms] = useState([]);
+  const [unreadMessages, setUnreadMessages] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/users/dashboard", { withCredentials: true })
+      .then(({ data }) => {
+        setUser(data.user);
+        setRooms(data.rooms);
+        setUnreadMessages(data.unreadMessages);
+      })
+      .catch((err) => {
+        console.error("Error:", err.response ? err.response.data : err);
+      });
+  }, []);
+
   return (
     <div className="sidebar">
       <h2>Your Rooms</h2>
       <ul>
         {rooms.map((room) => (
           <li key={room._id}>
-            <Link to="#" onClick={() => setSelectedRoom(room._id)}>
+            <Link to="#" >
               {room.name}
               {unreadMessages[room._id] > 0 && (
                 <span className="badge">
@@ -22,7 +41,7 @@ const Sidebar = ({ rooms, unreadMessages, setSelectedRoom }) => {
       </ul>
 
       {/* Link to create a new room page */}
-      <Link to="/create-room">
+      <Link to="/room">
         <button>Create New Room</button>
       </Link>
 
