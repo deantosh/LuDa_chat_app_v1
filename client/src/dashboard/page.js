ccfbd6fd912d0b8bff16691cd6bf0570393b027c
deantosh <deantoshdaiddoh@gmail.com>
2025-01-14 09:12:54 +0000

    Updated: fetched user object and pass as prop to child components

--- client/src/dashboard/page.js
+++ client/src/dashboard/page.js
@@ -3,18 +3,35 @@ import Sidebar from "../components/sidebar";
 import Header from "../components/header";
 import Chat from "../components/chat";
 import "../styles/dashboard.css";
+import axios from "axios";
 
 const Dashboard = () => {
+  const [user, setUser] = useState(""); // To store the logged-in user ID
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/users/me", {
+          withCredentials: true,
+        });
+        setUser(response.data.user); // Set the logged-in user ID
+      } catch (error) {
+        console.error("Failed to fetch user:", error);
+        alert("Failed to fetch user details.");
+      }
+    };
+    fetchUser();
+  }, []); // Runs only once on component mount
+    
   return (
     <>
       {/*Header component*/}
-      <Header />
+      <Header user={user} />
       <div className="dashboard">
         {/*Sidebar component*/}
         <Sidebar />
         {/*Chat component*/}
         <div className="chat">
-          <Chat/>
+          <Chat user={user} />
           <p>Select a room to view messages.</p>
         </div>
       </div>
