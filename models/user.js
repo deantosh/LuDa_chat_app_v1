d51c84f21225c80986af15c7a53af1edaeb7b09f
deantosh <deantoshdaiddoh@gmail.com>
2025-01-20 16:16:05 +0000

    Update: users collection -- add a new field user bio

--- models/user.js
+++ models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   avatar: { type: String },
+  bio: { type: String },
   status: { type: String, default: 'Available' },
   isOnline: { type: Boolean, default: false },
 });
