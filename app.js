5f1b2686da4d77452dfb09100876370b0143cf7d
deantosh <deantoshdaiddoh@gmail.com>
2025-01-22 00:35:28 +0000

    Initialize socket on the backend

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const router = require('./routes/index');
 const cors = require('cors');
+const http = require('http');
+const { setupSocket } = require('./socket');
 
 const app = express();
+const server = http.createServer(app);
+setupSocket(server);
 
+// Middleware
 app.use(cors({
   origin: 'http://localhost:3000', // Allow requests from your frontend
   methods: ['GET', 'POST', 'PUT', 'OPTIONS', 'DELETE'], // Allow the necessary HTTP methods
@@ -16,13 +21,14 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
+
+// Middleware to log origin of requests
 app.use((req, res, next) => {
   console.log('Request Origin:', req.get('Origin'));
   next();
 });
-// app.options('*', cors()); // Remove to avoid overriding your specific CORS configuration
 
 const PORT = 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
